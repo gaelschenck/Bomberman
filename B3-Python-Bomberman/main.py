24888a7bcf96c1303bc886da6e7345f0dfbdfc20
gaels <128142604+gaelschenck@users.noreply.github.com>
2024-10-23 15:48:03 +0200

    maj 16h

--- B3-Python-Bomberman/main.py
+++ B3-Python-Bomberman/main.py
@@ -9,23 +9,24 @@ def main():
     board.display()
     
     # Créer un joueur
-    player = Player(name="Alice", start_row=2, start_col=2)
+    player = Player(name="Bomberman", start_row=2, start_col=2)
     
     # Afficher la position initiale du joueur
     print(player)
     
     # Déplacer le joueur
-    player.move_up()
-    print(player)  # Devrait être à (1, 2)
+    if keyboard.is_pressed('z'):
+        player.move_up()
+        print(player)  # Devrait être à (1, 2)
 
-    player.move_left()
-    print(player)  # Devrait être à (1, 1)
+        player.move_left()
+        print(player)  # Devrait être à (1, 1)
 
-    player.move_down(board.rows)
-    print(player)  # Devrait être à (2, 1)
+        player.move_down(board.rows)
+        print(player)  # Devrait être à (2, 1)
 
-    player.move_right(board.cols)
-    print(player)  # Devrait être à (2, 2)  
+        player.move_right(board.cols)
+        print(player)  # Devrait être à (2, 2)  
     
     # Afficher le plateau mis à jour
     board.display()
