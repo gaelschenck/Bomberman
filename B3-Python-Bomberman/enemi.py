38d4a79b671690a6d236b74724051826ee29ee31
tojo2803 <ratsimbazafytojoniaina2803@gmail.com>
2025-01-13 15:35:05 +0100

    j'ai rendu plus lisble le code  dans enemy.py et main.py

--- B3-Python-Bomberman/enemi.py
+++ B3-Python-Bomberman/enemi.py
@@ -1,50 +1,68 @@
+"""
+definition des mouvements enemi et de la partie perdue si on se fait toucher par l'ennemi
+"""
+
 import random
 
-# définition des mouvements
-def move_up(map,position,enemi):
-        if map[position['x'] - 1][position['y']] == ' ' :
-           map[position['x']][position['y']] = ' '
-           map[position['x'] - 1][position['y']] = enemi
-           position['x'] = position['x']  - 1  
+def move_up(map, position, enemi):
+    """
+    deplace l'ennemi vers le haut.
+    """
+    if map[position['x'] - 1][position['y']] == ' ':
+        map[position['x']][position['y']] = ' '
+        map[position['x'] - 1][position['y']] = enemi
+        position['x'] = position['x'] - 1
 
-def move_left(map,position,enemi):
-    if map[position['x']][position['y'] - 1] == ' ' :
+def move_left(map, position, enemi):
+    """
+    deplace l'enemi vers le gauche.
+    """
+    if map[position['x']][position['y'] - 1] == ' ':
         map[position['x']][position['y']] = ' '
         map[position['x']][position['y'] - 1] = enemi
-        position['y'] = position['y']  - 1 
+        position['y'] = position['y'] - 1
 
-def move_right(map,position,enemi):
-    if map[position['x']][position['y'] + 1] == ' ' :
+def move_right(map, position, enemi):
+    """
+    deplace l'enemi vers le droite.
+    """
+    if map[position['x']][position['y'] + 1] == ' ':
         map[position['x']][position['y']] = ' '
-        map[position['x']][position['y'] + 1 ] = enemi
-        position['y'] = position['y']  + 1   
+        map[position['x']][position['y'] + 1] = enemi
+        position['y'] = position['y'] + 1
 
-
-def move_dwon(map,position,enemi):
-    if map[position['x'] + 1][position['y']] == ' ' :
+def move_down(map, position, enemi):
+    """
+    deplace l'enemi vers le bas.
+    """
+    if map[position['x'] + 1][position['y']] == ' ':
         map[position['x']][position['y']] = ' '
         map[position['x'] + 1][position['y']] = enemi
-        position['x'] = position['x']  + 1
+        position['x'] = position['x'] + 1
 
-# utilisation des nombres aléatoires pour faire bouger les ennemis        
-def move_enemi(map,position,enemi):
-    number_random = random.randint(0,3)
-    
-    if number_random == 0 :
-        move_dwon(map,position,enemi)
-    elif number_random == 1 :
-        move_left(map,position,enemi)
-    elif number_random == 2 :
-        move_right(map,position,enemi)
-    else :
-        move_up(map,position,enemi)
+def move_enemy(map, position, enemi):
+    """
+    deplace l'enemi aleatoirement soit vers le gauche,droite,haut ou bas.
+    """
+    number_random = random.randint(0, 3)
 
-# partie perdue si on se fait toucher par l'ennemi
-def attraper_par_enemi(map,position):  
+    if number_random == 0:
+        move_down(map, position, enemi)
+    elif number_random == 1:
+        move_left(map, position, enemi)
+    elif number_random == 2:
+        move_right(map, position, enemi)
+    else:
+        move_up(map, position, enemi)
+
+def attraper_par_enemi(map, position):
+    """
+    retourne True si le joueur se fait toucher par l'ennemi.
+    """
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (1, -1), (-1, -1), (-1, 1), (1, 1)]
     for dx, dy in directions:
         nx, ny = position["x"] + dx, position["y"] + dy
-        if map[nx][ny] == "p" :
+        if map[nx][ny] == "p":
             return True
-    else:
-        return False
+    return False
+
--- B3-Python-Bomberman/main.py
+++ B3-Python-Bomberman/main.py
@@ -1,83 +1,86 @@
-from sty import fg, RgbFg, Style
+"""
+Point d'entrée du jeu Bomberman
+"""
 import time
+from sty import fg, RgbFg, Style
 import keyboard
-from map import creat_map,display_map
-from player import move_dwon,move_left,move_right,move_up,place_bomb
-from enemi import move_enemi
-from enemi import attraper_par_enemi
+from map import creat_map, display_map
+from player import move_dwon, move_left, move_right, move_up, place_bomb
+from enemi import move_enemi, attraper_par_enemi
 from score import count_score
+
 
 # initialisation couleur
 fg.orange = Style(RgbFg(255, 150, 50))
-fg.rouge = Style(RgbFg(255,0,0))
-fg.bleu = Style(RgbFg(0,0,255))
+fg.rouge = Style(RgbFg(255, 0, 0))
+fg.bleu = Style(RgbFg(0, 0, 255))
+
 
 # definition des variables
-case_vide = " "
-mur_cassable = fg.orange + '|' + fg.rs
-mur_incassable = "|"
-score = 0
-player = "p"
-enemi = {
-        "e1" :fg.rouge + "E" + fg.rs
-      }
-bomb = "*"
-position_player = {
-    "x" : 1,
-    "y" : 1
+CASE_VIDE = " "
+MUR_CASSABLE = fg.orange + '|' + fg.rs
+MUR_INCASSABLE = "|"
+SCORE = 0
+PLAYER = "p"
+ENEMI = {
+    "e1": fg.rouge + "E" + fg.rs
 }
-bordure_gauche_droite = fg.bleu +  "|" + fg.rs
-bordure_bas_haut = fg.blue + "_" + fg.rs
+BOMB = "*"
+POSITION_PLAYER = {
+    "x": 1,
+    "y": 1
+}
+BORDURE_GAUCHE_DROITE = fg.bleu + "|" + fg.rs
+BORDURE_BAS_HAUT = fg.blue + "_" + fg.rs
+MAP = []
 
 
-
-#menu du jeu
+# menu du jeu
 print("--------------------------------MODE DE JEU-----------------------------------------")
 print("1- MODE HISTOIRE")
 mode_jeu = input()
-#------
-if int(mode_jeu) == 1 : 
-   dimension  = int(input("entrer la dimension du map: "))
-   map = creat_map(dimension,mur_cassable,mur_incassable,bordure_gauche_droite,bordure_bas_haut) 
-   position_enemi = {
-    "x" : int(dimension) - 2,
-    "y" : int(dimension) - 2
-    }  
-   compteur_boucle = 0
-   while True:
-       
-        fin_du_jeu = attraper_par_enemi(map,position_enemi)
-        map = display_map(map,position_player,player,position_enemi,enemi["e1"])
-        print("q: move_left, z: move_top, d: move_right, s: move_dwon, e: placer une bombe: ") 
+
+# ------
+if int(mode_jeu) == 1:
+    dimension = int(input("entrer la dimension du map: "))
+    MAP = creat_map(
+        dimension, MUR_CASSABLE, MUR_INCASSABLE, BORDURE_GAUCHE_DROITE, BORDURE_BAS_HAUT
+        )
+    position_enemi = {
+        "x": int(dimension) - 2,
+        "y": int(dimension) - 2
+    }
+    COMPTEUR_BOUCLE = 0
+    while True:
+        FIN_JEU = attraper_par_enemi(MAP, position_enemi)
+        MAP = display_map(MAP, POSITION_PLAYER, PLAYER, position_enemi, ENEMI["e1"])
+        print("q: move_left, z: move_top, d: move_right, s: move_dwon, e: placer une bombe: ")
         if keyboard.is_pressed('q'):
-            move_left(map, position_player, player)
-            score = count_score(score)
+            move_left(MAP, POSITION_PLAYER, PLAYER)
+            SCORE = count_score(SCORE)
         if keyboard.is_pressed('s'):
-            move_dwon(map, position_player,player)
-            score = count_score(score)
+            move_dwon(MAP, POSITION_PLAYER, PLAYER)
+            SCORE = count_score(SCORE)
         if keyboard.is_pressed('d'):
-            move_right(map,position_player,player)
-            score = count_score(score)
+            move_right(MAP, POSITION_PLAYER, PLAYER)
+            SCORE = count_score(SCORE)
         if keyboard.is_pressed('z'):
-            move_up(map,position_player,player)
-            score = count_score(score)
+            move_up(MAP, POSITION_PLAYER, PLAYER)
+            SCORE = count_score(SCORE)
         if keyboard.is_pressed('e'):
-             place_bomb(map,position_player,bomb,enemi,mur_cassable)
-             
-        if not(fin_du_jeu):
-            if compteur_boucle % 10 == 0:
-                move_enemi(map,position_enemi,enemi["e1"])   
-            compteur_boucle += 1    
-        if fin_du_jeu:
+            place_bomb(MAP, POSITION_PLAYER, BOMB, ENEMI, MUR_CASSABLE)
+
+        if not FIN_JEU:
+            if COMPTEUR_BOUCLE % 10 == 0:
+                move_enemi(MAP, position_enemi, ENEMI["e1"])
+            COMPTEUR_BOUCLE += 1
+        if FIN_JEU:
             print("--------------------------------------------------------vous avez perdu------------------------------------------------")
-            print(f"---score:{score}")
+            print(f"---score:{SCORE}")
             break
-        if enemi["e1"] == " ":
+        if ENEMI["e1"] == " ":
             time.sleep(1)
             print("--------------------------------------------------------vous avez gagné------------------------------------------------")
-            print(f"---score:{score}")
+            print(f"---score:{SCORE}")
             break
-        time.sleep(0.1)
-
-      
-
+        time.sleep(0.1)      
